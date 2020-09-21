7f7e1e9cc43f126159eb7a5e4ca7118678ac3a03
a.lobachyov <a.lobachyov@zebrains.team>
2020-09-21 16:10:25 +0400

    add sign out

--- app/src/main/java/com/riyga/github/MainActivity.kt
+++ app/src/main/java/com/riyga/github/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_favorites
+                R.id.navigation_home, R.id.navigation_favorites, R.id.navigation_user
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/riyga/github/UserFragment.kt
+++ app/src/main/java/com/riyga/github/UserFragment.kt
@@ -1,59 +1,59 @@
 package com.riyga.github
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.riyga.github.model.Params
+import com.riyga.github.model.User
+import io.realm.Realm
+import kotlinx.android.synthetic.main.fragment_user.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UserFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UserFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        realm = Realm.getDefaultInstance()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val user = realm.where(User::class.java).findFirst()
+        val userPic = activity?.findViewById<ImageView>(R.id.user_pic)
+
+        if (userPic != null) {
+            Glide.with(view).load(user?.avatar).into(userPic)
+        }
+        user_login.text = user?.login
+
+        sign_out.setOnClickListener{
+            logOut()
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        realm.close()
+    }
+
+    private fun logOut(){
+        realm.executeTransaction{
+            it.where(User::class.java).findFirst()?.deleteFromRealm()
+            it.where(Params::class.java).findFirst()?.deleteFromRealm()
+        }
+        this.startActivity(Intent(context, LoginActivity::class.java))
+        activity?.finish()
     }
 }
